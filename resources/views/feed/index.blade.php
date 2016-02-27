6840821be1d78d8eb63da4a92d2288f95ea8aa56
Gadurp1 <anthony@anthonys-MacBook-Pro.local>
2016-02-27 14:18:52 -0600

    Google Analytics Tag

--- resources/views/feed/index.blade.php
+++ resources/views/feed/index.blade.php
@@ -4,8 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Donald Trump Gifs | </title>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+      ga('create', 'UA-74373569-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
